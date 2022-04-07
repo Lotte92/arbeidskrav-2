8d67b2fe1264f3da550c9a8ce6ac904ec97837b3
c1ntani <cintani_muljono@hotmail.com>
2022-04-07 15:13:41 +0200

    Oppdatert js, css, html

---
+++ task3.js
@@ -0,0 +1,67 @@
+let getCharacters = async () => {
+  fetch("http://hp-api.herokuapp.com/api/characters")
+    .then((response) => {
+      console.log(response);
+      if (response.ok) {
+        return response.json();
+      }
+      throw Error("ERROR");
+    })
+    .then((data) => {
+      filterStaff(data);
+    })
+    .catch((err) => console.log("first", err));
+};
+getCharacters();
+let staff = [];
+
+function filterStaff(data) {
+  staff = data.filter(function (data) {
+    return data.hogwartsStaff == true;
+  });
+  showStaff(staff);
+  return staff;
+}
+
+function showStaff(staff) {
+  console.log(staff);
+  let staffList = document.querySelector(".staff-list");
+  staffList.innerHTML = "";
+  for (let i = 0; i < staff.length; i++) {
+    let staffName = document.createElement("h3");
+    let editContent = document.createElement("p");
+    let staffCard = document.createElement("p");
+    staffName.innerText = staff[i].name;
+    let staffHouse = document.createElement("p");
+    staffHouse.classList.add("staff-house");
+    staffHouse.innerText = `House: ${staff[i].house}`;
+    if (staff[i].house == "Gryffindor") {
+      staffCard.classList.add("staff-gryffindor");
+    } else if (staff[i].house == "Slytherin") {
+      staffCard.classList.add("staff-slytherin");
+    } else if (staff[i].house == "Hufflepuff") {
+      staffCard.classList.add("staff-hufflepuff");
+    } else if (staff[i].house == "Ravenclaw") {
+      staffCard.classList.add("staff-ravenclaw");
+    }
+
+    let staffPatronus = document.createElement("p");
+    staffPatronus.classList.add("staff-patronus");
+    if (staff[i].patronus == "") {
+      staffPatronus.innerText = "Patronus: Unknown";
+    } else {
+      staffPatronus.innerText = `Patronus: ${staff[i].patronus}`;
+    }
+
+    let staffImg = document.createElement("img");
+    staffImg.classList.add(".staff-img");
+    if (staff[i].image == "") {
+      staffImg.src = "/assets/default-img.jpeg";
+    } else {
+      staffImg.src = staff[i].image;
+    }
+
+    staffList.append(staffCard, editContent);
+    staffCard.append(staffName, staffHouse, staffPatronus, staffImg);
+  }
+}
