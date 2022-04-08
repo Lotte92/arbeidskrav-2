7a6b40119257c5589437b5ca4d66a234c6acb1b5
c1ntani <cintani_muljono@hotmail.com>
2022-04-08 21:30:49 +0200

    Stylet cards og house

--- task3.js
+++ task3.js
@@ -10,6 +10,7 @@ let getCharacters = async () => {
     .then((data) => {
       filterStaff(data);
     })
+
     .catch((err) => console.log("first", err));
 };
 getCharacters();
@@ -27,24 +28,33 @@ function showStaff(staff) {
   console.log(staff);
   let staffList = document.querySelector(".staff-list");
   staffList.innerHTML = "";
+
   for (let i = 0; i < staff.length; i++) {
+    let editCard = document.createElement("li");
+    editCard.classList.add("staff");
+    editCard.style.display = "none";
+
     let staffName = document.createElement("h3");
-    let editCard = document.createElement("ul");
-    let staffCard = document.createElement("ul");
     staffName.innerText = staff[i].name;
+    let staffCard = document.createElement("li");
     let staffHouse = document.createElement("p");
     staffHouse.classList.add("staff-house");
     staffHouse.innerText = `House: ${staff[i].house}`;
-    if (staff[i].house == "Gryffindor") {
+
+    //House
+    if (staff[i].house == "Ravenclaw") {
+      staffCard.classList.add("staff-ravenclaw");
+    } else if (staff[i].house == "Gryffindor") {
       staffCard.classList.add("staff-gryffindor");
     } else if (staff[i].house == "Slytherin") {
       staffCard.classList.add("staff-slytherin");
     } else if (staff[i].house == "Hufflepuff") {
       staffCard.classList.add("staff-hufflepuff");
-    } else if (staff[i].house == "Ravenclaw") {
-      staffCard.classList.add("staff-ravenclaw");
+    } else {
+      staffCard.classList.add("staff");
     }
 
+    //Patronus
     let staffPatronus = document.createElement("p");
     staffPatronus.classList.add("staff-patronus");
     if (staff[i].patronus == "") {
@@ -52,7 +62,6 @@ function showStaff(staff) {
     } else {
       staffPatronus.innerText = `Patronus: ${staff[i].patronus}`;
     }
-
     let staffImg = document.createElement("img");
     staffImg.classList.add(".staff-img");
     if (staff[i].image == "") {
